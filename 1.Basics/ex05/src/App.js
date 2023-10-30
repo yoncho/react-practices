6f8ab4bb7d6da2b111c7f7b10ce5ed8176dab43e
yoncho <joyonghyeon13@gmail.com>
2023-10-30 16:26:23 +0900

    react api

--- 1.Basics/ex05/src/App.js
+++ 1.Basics/ex05/src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
 function App(){
-    const App = document.createElement('div');
-    App.textContent = "hello webpack :D";
+    // const App = document.createElement('div');
+    // App.textContent = "hello webpack :D";
+    
+    React.createElement('div', null, 'hello react api : )');
     return App;
+
+    //JSX (위 REACT 코드와 100% 동일..!) 
+    // return (
+    //     <div>
+    //         hello react api world :D
+    //         <ul>
+    //             {
+    //                 ['1st', '2nd'].map((e)=>{
+    //                     return <li>${e}</li>;
+    //                 })
+    //             }
+    //         </ul>
+    //     </div>
+    // )
 }
 
-export {App};
+export {App};                                                                                                                                   
--- 1.Basics/ex05/src/index.js
+++ 1.Basics/ex05/src/index.js
@@ -1,3 +1,6 @@
 import { App } from "./App.js";
+import ReactDOM from 'react-dom';
+// document.getElementById('root').appendChild(App());
 
-document.getElementById('root').appendChild(App());
+ReactDOM.render(App(), document.getElementById('root'));
+// react api 사용!
