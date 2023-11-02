46740d9b3f0f9c8bc4cbeb19ff4fb72e450d3237
yoncho <joyonghyeon13@gmail.com>
2023-11-02 13:24:36 +0900

    event final

--- 2.Component/ex03/src/05/App.js
+++ 2.Component/ex03/src/05/App.js
@@ -2,12 +2,16 @@ import React, { useRef } from 'react';
 import './assets/scss/App.scss';
 
 export default function App() {
-    const outterRef = useRef(null);
+    const outerRef = useRef(null);
     const innerRef = useRef(null);
     
     return (
         <div
-            className={'App'}>
+            ref={outerRef}
+            className={'App'}
+            onScroll={e=>{
+                console.log(outerRef.current.scrollTop,outerRef.current.clientHeight, innerRef.current.clientHeight);
+            }}>
             <div
                 ref={ innerRef }>
                 <ul>
