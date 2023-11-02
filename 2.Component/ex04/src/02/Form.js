0f47132bc57c51757fc38280435e4c98d31c9e14
yoncho <joyonghyeon13@gmail.com>
2023-11-02 15:47:37 +0900

    state hook/ setState

--- 2.Component/ex04/src/02/Form.js
+++ 2.Component/ex04/src/02/Form.js
@@ -2,27 +2,59 @@ import React, { useState } from 'react';
 import './assets/Form.css';
 
 export default function Form() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [validEamil, setValidEmail] = useState(false);
+    const [gender, setGender] = useState('female');
+    const [birthYear, setBirthYear] = useState('');
+    
+    const onChangeName =(e)=>{
+        // setName(e.target.value);
 
+        //10자 제한
+        setName(e.target.value.substr(0, 10));
+    };
+
+    //비제어!
+    const onChangeEmail =(e)=>{
+        setEmail(e.target.value);
+        //check email format (include @)
+        const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        setValidEmail(re.test(email));
+    };
 
     return (
         <form id="joinForm" name="joinForm" method="post" action="/do/not/post">
             <label htmlFor="name">이름</label>
-            <input id="name" name="name" type="text" value={ "" } />
+            <input id="name" name="name" type="text" value={name} onChange={onChangeName}/>
 
             <label htmlFor="email">이메일</label>
-            <input id="email" name="email" type="text" value={ "" }/>
+            <input id="email" name="email" type="text" value={email} onChange={onChangeEmail}/>
+            {
+                email =='' ? 
+                    null:  
+                    validEamil? <b>o</b>: 
+                    <b>x</b>
+            }
 
             <label htmlFor="password">패스워드</label>
-            <input id="password" name="password" type="password" value={ "" } />
+            <input id="password" name="password" type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
 
             <fieldset>
                 <legend>성별</legend>
-                <label>여</label> <input type="radio" name="gender" value={ "female" } defaultChecked={ true } />
-                <label>남</label> <input type="radio" name="gender" value={ "male" } defaultChecked={ false } />
+                <label>여</label> <input type="radio" name="gender" value={'female'} defaultChecked={gender === 'female'} onChange={e=>setGender(e.target.value)} />
+                <label>남</label> <input type="radio" name="gender" value={'male'} defaultChecked={ gender === 'male' } onChange={e=>setGender(e.target.value)} />
             </fieldset>
 
             <label htmlFor="birthYear">생년</label>
-            <select id="birthYear">
+            <select 
+
+                id="birthYear"
+                value={birthYear}
+                onChange={e=> setBirthYear(e.target.value)}
+                >
+                <option value=''>생년을 선택하세요.</option>
                 <option value='1984'>1984년</option>
                 <option value='1985'>1985년</option>
                 <option value='1986'>1986년</option>
