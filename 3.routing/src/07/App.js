34ef708043cfc26c29d1786603b85265b70c5c5f
yoncho <joyonghyeon13@gmail.com>
2023-11-09 10:13:20 +0900

    route #finish

--- 3.routing/src/07/App.js
+++ 3.routing/src/07/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route} from 'react-router';
+// import {BrowserRouter as Router} from 'react-router-dom';
+// import {Routes, Route} from 'react-router';
+import { useRoutes } from 'react-router';
+
 import Main from "./component/Main";
-import {Gallery} from "./gallery";
+// import {Gallery} from "./component/gallery";
 import Guestbook from "./component/Guestbook";
 import About from "./component/About";
 import Error404 from "./component/error/Error404";
 import {Login, Join, Settings} from './component/user';
 
 import './assets/scss/App.scss'
+import { SiteLayout } from './layout';
 
 export default function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Main />}/>
-                <Route path='/gallery' element={<Gallery />}/>
-                <Route path='/guestbook' element={<Guestbook />}/>
-                <Route path='/about' element={<About />}/>
-                <Route path='/user/join' element={<Join />}/>
-                <Route path='/user/login' element={<Login />}/>
-                <Route path='*' element={<Error404 />}/>
-            </Routes>
-        </Router>
-    );
+    return useRoutes([
+        { path:'/', element: <Main />},
+        // { path:'/gallery', element: <Gallery />},
+        { path:'/guestbook', element: <Guestbook />},
+        { path:'/about', element: <About />},
+        { path:'/user/join', element: <Join />},
+        { path:'/user/login', element: <Login />}
+    ]);
+
+    //08의 경우 아래와 같이 사용할 수 있음...!
+    useRoutes([{
+        path:'/', 
+        element: <SiteLayout />,
+        children: [
+            { path:'', element: <Main />},
+            { path:'guestbook', element: <Guestbook />},
+            { path:'about', element: <About />},
+            { path:'user/join', element: <Join />},
+            { path:'user/login', element: <Login />}
+        ]
+    }]);
 }
